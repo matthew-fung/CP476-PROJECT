f1419af3f9c17772e6a504570fa2534bf4d37494
ypang1997 <31942352+ypang1997@users.noreply.github.com>
2019-03-03 15:42:07 -0500

    WIP - play1: if user clicks the circle while it is red, they automatically lose the round. If they click within 1 second of the circle turning green, they win.

--- js/play2.js
+++ js/play2.js
@@ -4,11 +4,13 @@
 
 //TODO: Set this to false before production - this outputs each step to console
 let debug = true;
+var isCorrect = true;
+var bagelsClicked = 0;
 
 /**
  * Starts the game time countdown
  * @param: duration - game time (int)
- * @param: display - 
+ * @param: display -
  */
 function startCountdown(duration) {
     var timer = duration,
@@ -24,12 +26,14 @@ function startCountdown(duration) {
 };
 
 
-function puppyClickHandler() { 
+function puppyClickHandler() {
     console.log('puppy clicked');
+    isCorrect = false;
 }
 
 function bagelClickHandler() {
     alert('bagel clicked');
+    bagelsClicked++;
 }
 
 
@@ -43,8 +47,8 @@ var bagel3 = document.getElementById('bagel3');
 
 var score = 0;
 window.onload = function () {
-    
-    startCountdown(7);  
+
+    startCountdown(7);
     puppy1.addEventListener('click', puppyClickHandler);
     puppy2.addEventListener('click', puppyClickHandler);
     puppy3.addEventListener('click', puppyClickHandler);
@@ -52,6 +56,20 @@ window.onload = function () {
     bagel1.addEventListener('click', bagelClickHandler);
     bagel2.addEventListener('click', bagelClickHandler);
     bagel3.addEventListener('click', bagelClickHandler);
+
+    // if user clicks 1 puppy or more, they will not receive any points
+    if(!isCorrect) {
+      bagelsClicked = 0;
+    } else {
+      // if they didn't click all the puppies, they will receive no points either
+      if(bagelsClicked != 3) {
+        isCorrect = false;
+      }
+    }
+
+    if(debug == true) {
+      console.log("Does user receive points? " + isCorrect);
+    }
 
 
 };
--- js/play3.js
+++ js/play3.js
@@ -4,6 +4,9 @@
 
 //TODO: Set this to false before production - this outputs each step to console
 let debug = true;
+var isClicked = false;
+var isCorrect = false;
+var circle = document.querySelector('#circle');
 
 
 /**
@@ -14,14 +17,14 @@ let debug = true;
 function getRandomValue() {
     let maxValue = 7;
     let randomValue = Math.floor(Math.random() * Math.floor(maxValue));
-    return randomValue;
+    return randomValue + 1;
 };
 
 
 /**
  * Starts the game time countdown + changes circle color
  * @param: duration - game time (int)
- * @param: display - 
+ * @param: display -
  */
 function startCountdown(duration, circleTime) {
     var timer = duration,
@@ -30,11 +33,23 @@ function startCountdown(duration, circleTime) {
         seconds = parseInt(timer % 60, 10);
         document.querySelector('#questionTime').textContent = seconds;
         if (timer == circleTime) {
-            console.log('timer = circletime')
+            console.log('timer = circletime');
             document.querySelector('#circle').src = "assets/circles/green-circle.png";
+        } else {
+          document.querySelector('#circle').src = "assets/circles/red-circle.png";
         }
+        // user must click green circle within 2 seconds
+        if(timer <= circleTime && timer >= circleTime - 1 && isClicked) {
+          console.log("time: " + timer);
+          isCorrect = true;
+        }
+
         if (timer > 0) {
             timer--;
+        }
+
+        if(timer == 0 && debug) {
+          console.log("User passed this round: " + isCorrect);
         }
     }, 1000);
 };
@@ -42,19 +57,32 @@ function startCountdown(duration, circleTime) {
 
 
 function circleClickHandler() {
-    console.log('circle clicked');
+    // only records clicks if they are within the timeframe of circle being green
+    //console.log('circle clicked');
+    circleImgStr = document.querySelector('#circle').src;
+    if(circleImgStr.includes("green")) {
+      isClicked = true;
+    } else {
+      // if they click while it is red, they will be wrong
+      isClicked = false;
+      circle.removeEventListener('click', circleClickHandler);
+    }
+    if(debug) {
+      console.log("Clicked on green: " + isClicked);
+    }
+
 }
 
 
-
-var circle = document.querySelector('circle');
 
 window.onload = function () {
     var greenCircleTime = getRandomValue();
     console.log('green circle time: '+ greenCircleTime);
     startCountdown(7, greenCircleTime);
+    circle.addEventListener("click", circleClickHandler);
+    //console.log("timer: " + timer + ". Isclicked: " + isClicked);
 
-    
+
 
 
 };
