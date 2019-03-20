a0dc8373fcff57cd7ec18ea2f906ad3e7222f978
Matthew Fung <>
2019-03-20 15:04:10 -0400

    added sample trivia API call in game controller

--- controllers/playController.js
+++ controllers/playController.js
@@ -4,10 +4,31 @@
 
 // TODO: import game model
 // const Game = require('../models/game');
+const axios = require('axios');
+const TRIVIA_API_URL_TRUE_FALSE = 'https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean';
+const TRIVIA_API_URL_MULTIPLE_CHOICE = 'https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple';
+
 
 // GAME 1
 exports.game1GET = function(req,res) {
-    res.send('GAME 1 GET');
+    axios.get(TRIVIA_API_URL_TRUE_FALSE)
+    .then(response => {
+        if(response.data.response_code == 0) {
+            let triviaQuestion = response.data.results[0].question;
+            let triviaType = response.data.results[0].type;
+            let triviaCorrectAnswer = response.data.results[0].correct_answer;
+            let triviaIncorrectAnswer = response.data.results[0].incorrect_answers[0];
+            res.send(triviaQuestion);
+        }
+        
+        
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    // res.send('GAME 1 GET');
+    // res.send()
 };
 exports.game1POST = function(req,res) {
     res.send('GAME 1 POST');
