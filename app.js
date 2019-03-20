6114cfe54eed16e29b0d6c8b1ca8b595249c5928
DESKTOP-57A9VCV\Matt <fung2730@mylaurier.ca>
2019-03-19 23:55:34 -0400

    added controllers for routes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const indexRouter = require('./routes/index.js');
-const loginRouter = require('./routes/login.js');
-const registerRouter = require('./routes/register.js');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+const playRouter = require('./routes/play');
 
 // enable bodyParser to get params from GET/POST requests
 // example: https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/play', playRouter)
 
 
 app.use(express.static(path.join(__dirname, 'public')));
