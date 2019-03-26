5832ad4dad5b2acae7de6b409b501dc9e106f9d0
ypang1997 <31942352+ypang1997@users.noreply.github.com>
2019-03-26 17:43:37 -0400

    finished getting origin & destination - working on uber

--- public/js/destination.js
+++ public/js/destination.js
@@ -5,10 +5,15 @@
 //var map, infoWindow;
 var currentLat = null;
 var currentLng = null;
+var destLat = null;
+var destLng = null;
 var place = 'Wilfrid Laurier University'; // default location on start of map
 var destination = null;
 var current = null;
 var markersArray = [];
+var map;
+var service;
+var infowindow;
 
 function success(position) {
   var pos = {
@@ -19,6 +24,8 @@ function success(position) {
   currentLat = pos.lat;
   currentLng = pos.lng;
   current = {lat: currentLat, lng: currentLng};
+
+  getCoordinates();
   //getCoordinates();
 }
 
@@ -34,22 +41,32 @@ function getCurrentLocation() {
     alert("Location not found");
   }
 
+
 }
 
 function getCoordinates() {
+  // for debugging
 
+  // console.log('markers: ')
+  // for(var i = 0; i < markersArray.length; i++) {
+  //   console.log(markersArray[i].position.lat());
+  //   console.log(markersArray[i].position.lng());
+  // }
+
+  console.log("current: ");
   console.log(currentLat);
   console.log(currentLng);
+
+  console.log("destination: ");
+  console.log(destLat);
+  console.log(destLng);
+
   //initMap();
 }
 
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-var map;
-var service;
-var infowindow;
 
 function initMap() {
   markersArray = [];
@@ -67,9 +84,9 @@ function initMap() {
   // when user clicks map, a marker will be placed
   google.maps.event.addListener(map, 'click', function(event) {
        placeMarker(event.latLng);
+       console.log(event.latLng);
   });
 
-  // lol change this ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
   var request = {
     query: place,
@@ -80,16 +97,16 @@ function initMap() {
 
   service.findPlaceFromQuery(request, function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-
-      }
+      createMarker(results[0]);
 
       map.setCenter(results[0].geometry.location);
-      //////////////////////////////////////////////////////// FOUND DESINATION COORDINATES
+
+      // destination coordinates. on first run, it will be wilfrid laurier university
+      destLat = results[0].geometry.location.lat();
+      destLng = results[0].geometry.location.lng();
       destination = {
-        lat: results[0].geometry.location.lat(),
-        lng: results[0].geometry.location.lng()
+        lat: destLat,
+        lng: destLng
       };
 
 
@@ -99,11 +116,18 @@ function initMap() {
 
 // automatically generate marker
 function createMarker(place) {
+  // there can only be one destination, delete all from array
+  for(var i = 0; markersArray.length; i++) {
+    deleteMarker(markersArray[i]);
+  }
+  markersArray = [];
+
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
   markersArray.push(marker);
+
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
@@ -112,35 +136,64 @@ function createMarker(place) {
       deleteMarker(marker);
   });
 
-  destination = place.geometry.location;
-  console.log(markersArray);
+  // set destination
+  destLat = place.geometry.location.lat();
+  destLng = place.geometry.location.lng();
+  destination = {
+    lat: destLat,
+    lng: destLng
+  };
+  getCoordinates();
 
 }
 
 
 function placeMarker(location) {
+    for(var i = 0; markersArray.length; i++) {
+      deleteMarker(markersArray[i]);
+    }
+    markersArray = [];
     var marker = new google.maps.Marker({
         position: location,
         map: map
     });
 
     markersArray.push(marker);
-    // info on click
-    // google.maps.event.addListener(marker, 'click', function() {
-    //   infowindow.setContent(place.name);
-    //   infowindow.open(map, this);
-    //
-    //   // set destinationA
-    //
-    // });
+    var geocoder = new google.maps.Geocoder;
+    var address = null;
 
-    destination = location;
+    // lol idk if this works ahhahaepof'jaksdaj;akl
+    geocoder.geocode({
+      'latLng': location
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          address = results[0].formatted_address;
+        }
+      }
+    });
+
+
+
+    // info on click
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(address);
+      infowindow.open(map, this);
+    });
+
+    // set destination
+    destLat = location.lat();
+    destLng = location.lng();
+    destination = {
+      lat: destLat,
+      lng: destLng
+    };
 
     google.maps.event.addListener(marker,"dblclick", function() {
         deleteMarker(marker);
     })
 
-    console.log(markersArray);
+    getCoordinates();
 
 
 }
@@ -154,50 +207,23 @@ function deleteMarker(marker) {
   }
   marker.setMap(null);
   marker = null;
-  console.log(markersArray);
+  getCoordinates();
+
+  // MAKE SURE THAT THEY CANNOT
+  // if(markersArray.length == 0) {
+  //   alert('Please click on a location');
+  // }
+}
+
+function getUberQuote() {
+  if(markersArray.length == 0) {
+    alert("Please select a destination. If there is no pin on the map, you have not selected it.");
+  } else {
+    
+  }
 }
 
 
 
 
 // FOR UBER API REQUESTS, USE AXIOS AND HAVE THE UBER API URL AND QUERY
-
-
-
-
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 6
-//   });
-//   infoWindow = new google.maps.InfoWindow;
-//
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-//
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent('Location found.');
-//       infoWindow.open(map);
-//       map.setCenter(pos);
-//     }, function() {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-// }
-//
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(browserHasGeolocation ?
-//                         'Error: The Geolocation service failed.' :
-//                         'Error: Your browser doesn\'t support geolocation.');
-//   infoWindow.open(map);
-// }
