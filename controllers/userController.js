7211ada0167c206c6d7037b97050335f094e7283
franco-wong <wong9661@mylaurier.ca>
2019-04-02 18:28:36 -0400

    storing user registrations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 /**
  * userController.js - handles GET/POST actions for login/register
  */
+var crypto = require('crypto');
 
 // TODO: import user
 const User = require('../models/user-mongo');
@@ -8,64 +9,89 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 // REGISTER
-exports.registerGET = function(req, res) {
+exports.registerGET = function (req, res) {
     res.render('register');
 };
 
-exports.registerPOST = function(req, res) {
+exports.registerPOST = function (req, res) {
     // TODO: handle register POST
     let user = new User();
-    user.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
+
+    var passSalt = generateSalt();
+    var hash = crypto.createHmac('sha1', passSalt).update(req.body.password).digest('hex');
     
-    user.save(function(error){
-        if(error){
-            console.log("Register Error:"+error);
-            return;
+    console.log(passSalt);
+    console.log(hash);
+    
+    console.log(passSalt.typeof)
+    user.name = req.body.name;
+    user.password = hash;
+    user.salt = passSalt;
+    user.email = req.body.email;
+
+    user.save(function (error) {
+        if (error) {
+            console.log("Register Error:" + error);
+            res.redirect('/register');
         } else {
             console.log("Register Successful");
             res.redirect('/home');
         }
     });
-    
-//    res.send("name: " + name + "<br>" + "password: " + password + "<br>" + "email:" + email);
+
+    function generateSalt() {
+        var buf = crypto.randomBytes(16);
+        console.log(buf.typeof)
+        return buf.toString('hex');
+    }
 };
 
 
 // LOGIN
-exports.loginGET = function(req, res) {
+exports.loginGET = function (req, res) {
     res.render('login');
 };
-exports.loginPOST = function(req, res) {
+exports.loginPOST = function (req, res) {
     // TODO: authentication (need db to be set up first)
 
-//    user.save(function(error){
-//        if(error){
-//            console.log(error);
-//            return;
-//        } else {
-//            res.redirect('/');
-//        }
-//    });
+    User.findOne({
+        email: req.body.email
+    }, function (err, user) {
+        if (err) return handleError(err);
+        
+        var Hash = pbkdf2(req.body.password, user.passSalt);
+        
+//        var buf = Buffer.from(user.passSalt);
+        
+//        var hash = crypto.createHmac('sha1', buf).update(req.body.password).digest('hex');
+        
+        if (user.email == req.body.email && hash == user.password) {
+            console.log('%s %s %s', user.email, user.name, user.password);
+
+            res.redirect('/home');
+        } else {
+            console.log("Incorrect login info");
+            res.redirect('/login');
+        }
+    });
 };
 
 
 // UPDATE
-exports.updateGET = function(req, res) {
+exports.updateGET = function (req, res) {
     res.send('NOT IMPLEMENTED: user update GET');
 };
 
-exports.updatePOST = function(req, res) {
+exports.updatePOST = function (req, res) {
     res.send('NOT IMPLEMENTED: user update POST');
 };
 
 
 // DELETE
-exports.deleteGET = function(req, res) {
+exports.deleteGET = function (req, res) {
     res.send('NOT IMPLEMENTED: user delete GET');
 };
 
-exports.deletePOST = function(req, res) {
+exports.deletePOST = function (req, res) {
     res.send('NOT IMPLEMENTED: user delete POST');
 };
--- models/user-mongo.js
+++ models/user-mongo.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
+    salt: {type: String, unique: true, required: true},
     email: {type: String, unique:true, required: true, dropDups: true},
     accountCreation: {type: Date, default: Date.now}
 });
